2791a12cfb0b445c43f804a73f4020cc47b745c6
Dick Lieber <dick@u505.com>
2023-06-30 09:47:32 -0500

    Rc210Spec run sequentially

--- test/net/wa9nnn/rc210/serial/comm/Rc210Spec.scala
+++ test/net/wa9nnn/rc210/serial/comm/Rc210Spec.scala
@@ -18,53 +18,75 @@
 package net.wa9nnn.rc210.serial.comm
 
 import com.fazecast.jSerialComm.SerialPort
-import net.wa9nnn.rc210.fixtures.WithTestConfiguration
+import net.wa9nnn.RcSpec
 import net.wa9nnn.rc210.serial.comm.RcOperation.RcResponse
-import net.wa9nnn.rc210.serial.{ComPort, ComPortPersistence}
+import net.wa9nnn.rc210.serial.{ComPort, RcSerialPort, RcSerialPortManager}
 import org.mockito.Mockito.when
-import org.scalatest.TryValues
+import org.scalatest.TryValues.convertTryToSuccessOrFailure
+import org.scalatest.{Sequential, TryValues}
 import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.mockito.MockitoSugar.mock
 
-import scala.util.{Success, Try}
+import scala.util.{Failure, Success}
 
-class Rc210Spec extends WithTestConfiguration with MockitoSugar with TryValues{
+class Rc210Spec extends Sequential(
+  new SendOne(),
+  new SendBatch(),
+  new StartStopClose(),
+) with MockitoSugar with TryValues {
+}
 
-  def listPorts: Seq[ComPort] = {
-    val ports = SerialPort.getCommPorts
-    ports.map(ComPort(_)).toList
+class SendOne extends RcSpec {
+  val rcResponseSendOne: RcResponse = Rc210ForTest().sendOne("Version", "1GetVersion").triedResponse.success.value
+  rcResponseSendOne.head should equal("803")
+  rcResponseSendOne(1) should equal("+GETVE")
+}
+
+class SendBatch extends RcSpec {
+  val batchRespnses: Seq[RcOperationResult] = Rc210ForTest().sendBatch("Version", "1GetVersion", "1GetVersion").get.results
+  batchRespnses should have length (2)
+  val rcOperationResult: RcOperationResult = batchRespnses.head
+  val rcResponse: RcResponse = rcOperationResult.triedResponse.success.value
+  rcResponse.head should equal("803")
+  rcResponse(1) should equal("+GETVE")
+  println("sendBatch done")
+}
+
+class StartStopClose extends RcSpec {
+  Rc210ForTest().start match {
+    case Failure(exception) =>
+      throw exception
+    case Success(rcOperation: RcOperation) =>
+      val r1 = rcOperation.sendBatch("one", "1GetVersion", "1GetVersion")
+      val r2 = rcOperation.sendBatch("two", "1GetVersion", "1GetVersion")
+
+      r1.name should be("one")
+      r2.name should be("two")
+      rcOperation.close()
   }
+}
 
-  def ft232Port: ComPort = {
-    listPorts.find(_.friendlyName.contains("FT232")) match {
-      case Some(value) =>
-        value
-      case None =>
-        throw new IllegalStateException("Can't find FT232 port")
+object Rc210ForTest {
+  def apply(): Rc210 = {
+    def listPorts: Seq[ComPort] = {
+      val ports = SerialPort.getCommPorts
+      ports.map(ComPort(_)).toList
     }
-  }
 
-
-  "RC210" should {
-    val comPortPersistence = mock[ComPortPersistence]
-    when(comPortPersistence.currentComPort).thenReturn(Success(ft232Port))
-    val rc210 = new Rc210(comPortPersistence)
-    "handle one shot" in {
-      val rcOperationResult: RcOperationResult = rc210.sendOne("Version", "1GetVersion")
-      val rcResponse: RcResponse = rcOperationResult.triedResponse.success.value
-      rcResponse.head should equal("803")
-      rcResponse(1) should equal("+GETVE")
-
+    def ft232Port: ComPort = {
+      listPorts.find(_.friendlyName.contains("FT232")) match {
+        case Some(value) =>
+          value
+        case None =>
+          throw new IllegalStateException("Can't find FT232 port")
+      }
     }
-    "handle batch" in {
-      val triedOperationsResult: Try[BatchOperationsResult] = rc210.sendBatch("Version", "1GetVersion", "1GetVersion")
-      val operationsResult: BatchOperationsResult = triedOperationsResult.get
-      val rcRes: Seq[RcOperationResult] = operationsResult.results
-      rcRes should have length (2)
-      val rcOperationResult: RcOperationResult = rcRes.head
-      val rcResponse: RcResponse = rcOperationResult.triedResponse.success.value
-      rcResponse.head should equal("803")
-      rcResponse(1) should equal("+GETVE")
 
-    }
+    val rcSerialPortManager = mock[RcSerialPortManager]
+    val serialPort = SerialPort.getCommPort(ft232Port.descriptor)
+    val rcSerialPort = new RcSerialPort(serialPort)
+    when(rcSerialPortManager.serialPort()).thenReturn(rcSerialPort)
+    new Rc210(rcSerialPortManager)
+
   }
 }
